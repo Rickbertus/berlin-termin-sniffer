dbea0821beb5a0e2b6dd729a264e1be695e22e46
ricky flugrat <rft@dasburo.com>
2022-12-15 14:58:18 +0100

    chore: add Readme and Refactor Code

---
+++ README.md
@@ -0,0 +1,40 @@
+# Terminvereinbarung Berlin Behörde:
+
+
+Der Sniffer schaut in allen Ämter nach einem freien Termin der nächsten 2 Monate
+
+1. einmalig den Link aufrufen
+https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen[]=121591&dienstleisterlist=122210,122217,122219,122227,122231,122238,122243,122252,122260,122262,122254,122271,122273,122277,122280,122282,122284,122291,122285,122286,122296,150230,122294,122312,122314,122304,122311,122309,122281,122283,122279,122276,122274,122267,122246,122251,122257,122208,122226&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F121591%2F
+
+2. Software installieren und ausführen
+
+    * ````npm install````
+
+    * ````npm run start````
+
+3. Logfile auswerten
+   
+```
+[13:38:02] -  KEIN TERMIN VERFÜGBAR
+[13:38:32] -  KEIN TERMIN VERFÜGBAR
+[13:39:02] -  KEIN TERMIN VERFÜGBAR
+[13:40:01] -  KEIN TERMIN VERFÜGBAR
+[13:40:31] -  KEIN TERMIN VERFÜGBAR
+[13:41:01] -  KEIN TERMIN VERFÜGBAR
+[13:41:32] -  KEIN TERMIN VERFÜGBAR
+[13:42:02] -  KEIN TERMIN VERFÜGBAR
+[13:42:32] -  KEIN TERMIN VERFÜGBAR
+[13:43:02] -  KEIN TERMIN VERFÜGBAR
+[13:43:32] -  KEIN TERMIN VERFÜGBAR
+[13:44:01] -  KEIN TERMIN VERFÜGBAR
+[13:44:31] -  KEIN TERMIN VERFÜGBAR
+[13:45:01] -  KEIN TERMIN VERFÜGBAR
+[13:45:31] -  KEIN TERMIN VERFÜGBAR
+[13:46:01] -  TERMIN VERFÜGBAR AM:
+[13:46:01] -  TERMIN VERFÜGBAR AM:  15
+---> LINK:  https://service.berlin.de/terminvereinbarung/termin/time/1671058800/
+[13:46:32] -  KEIN TERMIN VERFÜGBAR
+[13:47:03] -  KEIN TERMIN VERFÜGBAR
+
+```
+
--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const cron = require("node-cron");
 
-async function main() {
-    const url = "https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen=121591&dienstleisterlist=122210,122217,122219,122227,122231,122238,122243,122252,122260,122262,122254,122271,122273,122277,122280,122282,122284,122291,122285,122286,122296,150230,122294,122312,122314,122304,122311,122309,122281,122283,122279,122276,122274,122267,122246,122251,122257,122208,122226&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F121591%2F";
-
+async function getSessionCookie(url) {
     let cookie = "";
     await axios.get(url, {
         maxRedirects: 0,
@@ -16,7 +14,14 @@ async function main() {
         //console.log('COOKIE', error.response.headers['set-cookie'][0]);
         cookie = error.response.headers['set-cookie'][0];
     });
+    return cookie;
+}
 
+function timeStamp() {
+    return '[' + new Date().toISOString().substring(11, 19) + '] - ';
+}
+
+async function sniffAppointment(url, cookie) {
     await axios.get(url, {
         headers: {
             "Cookie": cookie
@@ -28,22 +33,32 @@ async function main() {
             let $buchbar = $(".buchbar");
             if ($buchbar.get().length > 1) {
                 $buchbar.each((i, el) => {
-                    console.log("TERMIN VERFÜGBAR AM: ", $(el).text());
+                    console.log(timeStamp(), "TERMIN VERFÜGBAR AM: ", $(el).text());
                     $(el).find("a").each((i, link) => {
-                        console.log("LINK: ", "https://service.berlin.de" + $(link).attr("href"));
+                        console.log("---> LINK: ", "https://service.berlin.de" + $(link).attr("href"));
                     });
                 });
             } else {
-                console.error("KEIN TERMIN VERFÜGBAR");
+                console.error(timeStamp(), "KEIN TERMIN VERFÜGBAR");
             }
         }
     }).catch((error) => {
         console.log('ERROR', error.response);
     });
+}
+
+async function main() {
+    const url = "https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen=121591&dienstleisterlist=122210,122217,122219,122227,122231,122238,122243,122252,122260,122262,122254,122271,122273,122277,122280,122282,122284,122291,122285,122286,122296,150230,122294,122312,122314,122304,122311,122309,122281,122283,122279,122276,122274,122267,122246,122251,122257,122208,122226&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F121591%2F";
+
+    // Get session cookie
+    let cookie = await getSessionCookie(url);
+
+    // Sniff your "Berlin Behörden Termin" every minute
+    await sniffAppointment(url, cookie);
 
 }
 
-cron.schedule('* 5-20 * * 1-5', () => {
+cron.schedule('*/30 * 5-20 * * 1-5', () => {
     //console.log('-- START CRON --');
     main().catch((e) => {
         console.error(e);
